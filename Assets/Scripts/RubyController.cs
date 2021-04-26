61ade173df609b253edd92b08456a88c256a60cd
park8306 <kosaf0804@gmail.com>
2021-04-26 19:44:45 +0900

    컨트롤러와 키보드 입력 단원

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -8,7 +8,8 @@ public class RubyController : MonoBehaviour
     // 가사가 다시 수정
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
     }
 
     // 화면 갱신될때마다 호출됨 - git테스트중
@@ -19,8 +20,8 @@ public class RubyController : MonoBehaviour
         Debug.Log(horizontal + "그리고 다른 부분을 추가 수정했다 - git test중");
 
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
